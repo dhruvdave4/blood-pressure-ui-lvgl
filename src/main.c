49e2caf68a8ed842271f2886d50681e09f58cf8b
Dhruv Dave <dhruvdave4620@gmail.com>
2026-06-14 17:59:17 +0000

    updated_files_and_program

--- src/main.c
+++ src/main.c
@@ -20,8 +20,8 @@
   #include <pthread.h>
 #endif
 #include "lvgl/lvgl.h"
-#include "lvgl/examples/lv_examples.h"
-#include "lvgl/demos/lv_demos.h"
+// #include "lvgl/examples/lv_examples.h"
+// #include "lvgl/demos/lv_demos.h"
 #include <SDL.h>
 
 #include "hal/hal.h" 
